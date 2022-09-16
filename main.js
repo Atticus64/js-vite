a8d7dfc0a2cea8861bc767d50ee78a73504051bb
Jona <jonathanelian64@gmail.com>
2022-09-16 14:36:12 -0600

    Change img woods and Add techlogies icons
    add functionality of hidden class

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 $ = selector => document.querySelector(selector)
 
 $button = $('.counter-btn')
+$pythonIcon = $('.py') 
 
 $button.addEventListener('click', () => {
-  console.log('click')
+  if ($pythonIcon.classList.contains('hidden')){
+    $pythonIcon.classList.toggle('hidden')
+  } else {
+    $pythonIcon.classList.toggle('hidden')
+  }
+
 })
